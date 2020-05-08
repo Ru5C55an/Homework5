8fab2b29be8acbd73ba1b94962b31decdd9c2db8
Ruslan Sadykov <S.Ru5C55an.N@gmail.com>
2020-05-08 10:31:28 +1000

    Удаление запроса изображений из папки blocks

--- src/js/index.js
+++ src/js/index.js
@@ -7,7 +7,6 @@ import '../scss/style.scss';
 
 import 'bootstrap';
 
-require.context('../blocks/', true, /\.(png|svg|jpg)$/);
 require.context('../img/', true, /\.(png|gif|svg|jpg)$/);
 
 $('#btn-tooltip').tooltip();
